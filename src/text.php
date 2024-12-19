1d520439f0e7437735e321e4a11b484c756db77b
Alec Tuchscherer <alec@onesourceimaging.com>
2024-12-19 02:17:33 -0600

    Change to Class Structure

--- src/text.php
+++ src/text.php
@@ -1,24 +1,26 @@
 <?php
-namespace Pyro100\CliUtils\Text;
-require_once('vendor/autoload.php');
-function Colored($text, $color) {
-    $colors = [
-        'black' => '0;30',
-        'dark_gray' => '1;30',
-        'blue' => '0;34',
-        'light_blue' => '1;34',
-        'green' => '0;32',
-        'light_green' => '1;32',
-        'cyan' => '0;36',
-        'light_cyan' => '1;36',
-        'red' => '0;31',
-        'light_red' => '1;31',
-        'purple' => '0;35',
-        'light_purple' => '1;35',
-        'brown' => '0;33',
-        'yellow' => '1;33',
-        'light_gray' => '0;37',
-        'white' => '1;37',
-    ];
-    return "\033[" . $colors[$color] . "m" . $text . "\033[0m";
+namespace Pyro100\CliUtils;
+class Text {
+
+    static function Colored($text, $color) {
+        $colors = [
+            'black' => '0;30',
+            'dark_gray' => '1;30',
+            'blue' => '0;34',
+            'light_blue' => '1;34',
+            'green' => '0;32',
+            'light_green' => '1;32',
+            'cyan' => '0;36',
+            'light_cyan' => '1;36',
+            'red' => '0;31',
+            'light_red' => '1;31',
+            'purple' => '0;35',
+            'light_purple' => '1;35',
+            'brown' => '0;33',
+            'yellow' => '1;33',
+            'light_gray' => '0;37',
+            'white' => '1;37',
+        ];
+        return "\033[" . $colors[$color] . "m" . $text . "\033[0m";
+    }
 }
