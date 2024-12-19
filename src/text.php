ddf4fb8463a648df5f214df073d7fd8f37f43f3e
Alec Tuchscherer <alec@onesourceimaging.com>
2024-12-19 02:43:06 -0600

    Add Text Color Enum

--- src/text.php
+++ src/text.php
@@ -26,4 +26,8 @@ class Text
 
         return "\033[" . $color->value . "m" . $text . "\033[0m";
     }
+    static function MoveCursor($x, $y)
+    {
+        return "\033[" . $y . ";" . $x . "H";
+    }
 }
